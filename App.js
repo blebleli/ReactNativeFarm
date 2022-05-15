180fa1519828196a81cbf07fdf439fe26531d294
blebleli <blebleli@naver.com>
2022-05-15 23:18:21 +0900

    LoginPage, design

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import LoginLogo from './components/LoginLogo';
 import LoginView from './components/LoginView';
 
+
 const Stack = createNativeStackNavigator();
+//const Drawer = createDrawerNavigator();
 
 function App() {
   return (
--- components/LoginView.js
+++ components/LoginView.js
@@ -1,35 +1,95 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, View,StyleSheet, TextInput, TouchableOpacity,Text,Image,Alert } from "react-native";
 
 const LoginView = () => {
   const [text, onChangeText] = React.useState("Useless Text");
   const [number, onChangeNumber] = React.useState(null);
-
+  const onPress = () =>
+    Alert.alert("Alert Title",
+                "My Alert Msg",)
   return (
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
+<SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.sectionStyle}>
+          <Image
+            source={ require('../image/loginIDButton.png')}
+            style={styles.imageStyle}
+          />
+          <TextInput
+            style={{flex: 1}}
+            placeholder="Enter Your ID Here"
+            placeholderTextColor = "white"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <View style={styles.sectionStyle}>
+          <Image
+            source={ require('../image/loginPWButton.png')}
+            style={styles.imageStyle}
+          />
+          <TextInput
+            style={{flex: 1}}
+            placeholder="Enter Your Password Here"
+            placeholderTextColor = "white"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <View style={styles.loginButtonStyle}>
+            <TouchableOpacity
+             style={styles.loginButton}
+             onPress={onPress}
+             >
+               <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 60,
   },
+  sectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E2F0D9',
+    height: 40,
+    borderRadius: 6,
+    margin: 10,
+  },
+  loginButtonStyle: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#92D050',
+      height: 40,
+      borderRadius: 6,
+      margin: 10,
+    },
+  imageStyle: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+  loginButton: {
+    flex: 1,
+    alignItems: "center",
+  },
+  loginText: {
+    color : 'white',
+    fontWeight: 'bold'
+  }
+
+
 });
 
 export default LoginView;
